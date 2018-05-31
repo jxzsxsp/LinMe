0185e8ca47a478c313a20d894c1738635fe8a563
Sean.Xiao <jxzsxsp@qq.com>
2018-05-31 22:41:39 +0800

    add 2048

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Game2048 from "./tests/2048/Game2048"
+
+export default class App extends Component {
+  render() {
+    return (
+        <Game2048/>
+    );
+  }
+}
---
+++ src/tests/Navigation.android.js
@@ -0,0 +1,14 @@
+import React, {
+    Component,
+} from 'react';
+import {
+    Text
+} from 'react-native';
+
+export default class Navigation extends Component {
+    render() {
+        return (
+            <Text>Android</Text>
+        );
+    }
+}
